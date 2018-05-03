1b5bf304230023f83203fba57f708954e539f36f
uk96 <uddeshkadu@gmail.com>
2018-05-03 19:33:07 +0530

    Exp 7 and 8 Unity 3D

---
+++ GAP/Exp 6 & 7/Roll_A_Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	int flag=0;
+	void Update () 
+	{
+		if (flag == 0) {
+			transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		}
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.name =="Player") {
+			Destroy(gameObject);
+			flag = 1;
+		}
+	}
+}
